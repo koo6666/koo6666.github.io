b48d4ad67d0492b51045882c5a15fb4593f74a8c
gujixian <gujixian@gmail.com>
2024-12-22 02:57:00 +0800

    Site updated: 2024-12-22 02:56:59

---
+++ 2024/12/22/谈一下服务端的关卡系统/101.mm
@@ -0,0 +1,114 @@
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="101" OBJECT="java.lang.Long|101" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1734258497810" STYLE="oval" CHILD_NODES_LAYOUT="TOPTOBOTTOM_RIGHT_FLOW">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_tags="UNDER_NODES" associatedTemplateLocation="template:/standard-1.6.mm" fit_to_viewport="false"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#afd3f7" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#afd3f7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="5" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="NPC" POSITION="bottom_or_right" ID="ID_698391684" CREATED="1734258499697" MODIFIED="1734258503491">
+<edge COLOR="#ff0000"/>
+<node TEXT="..." ID="ID_951994895" CREATED="1734259060899" MODIFIED="1734259062051"/>
+</node>
+<node TEXT="怪物" POSITION="top_or_left" ID="ID_200894490" CREATED="1734258591163" MODIFIED="1734258598647">
+<edge COLOR="#ff00ff"/>
+<node TEXT="..." ID="ID_997760884" CREATED="1734259063217" MODIFIED="1734259064215"/>
+</node>
+<node TEXT="场景" POSITION="top_or_left" ID="ID_1464042123" CREATED="1734258504847" MODIFIED="1734258601197">
+<edge COLOR="#0000ff"/>
+<node TEXT="tp" ID="ID_289555191" CREATED="1734258582073" MODIFIED="1734258604193"/>
+<node TEXT="..." ID="ID_736313820" CREATED="1734259066695" MODIFIED="1734259067807"/>
+</node>
+<node TEXT="规则" POSITION="top_or_left" ID="ID_777785078" CREATED="1734258510231" MODIFIED="1734258808990">
+<edge COLOR="#00ff00"/>
+<node TEXT="进入规则" POSITION="bottom_or_right" ID="ID_1576880906" CREATED="1734258530445" MODIFIED="1734258541634">
+<node TEXT="等级" ID="ID_1167372630" CREATED="1734258543208" MODIFIED="1734258971951"/>
+<node TEXT="组队" ID="ID_5672759" CREATED="1734258563140" MODIFIED="1734258567466"/>
+<node TEXT="同频道唯一" ID="ID_175814004" CREATED="1734258569173" MODIFIED="1734258573431"/>
+</node>
+<node TEXT="离开规则" ID="ID_1579513187" CREATED="1734258676121" MODIFIED="1734259095680">
+<node TEXT="清除相关道具" ID="ID_262875857" CREATED="1734260196139" MODIFIED="1734356090909"/>
+<node TEXT="方式" ID="ID_1565864321" CREATED="1734260212835" MODIFIED="1734260223293">
+<node TEXT="组队" POSITION="bottom_or_right" ID="ID_1621019909" CREATED="1734258843563" MODIFIED="1734258847612">
+<node TEXT="队员离开" ID="ID_1900642193" CREATED="1734259072227" MODIFIED="1734259076506"/>
+<node TEXT="队伍解散" ID="ID_1221037933" CREATED="1734259077786" MODIFIED="1734259083075"/>
+</node>
+<node TEXT="npc对话" POSITION="bottom_or_right" ID="ID_291463086" CREATED="1734259085305" MODIFIED="1734259095679"/>
+</node>
+</node>
+<node TEXT="奖励规则" ID="ID_1821597627" CREATED="1734258698696" MODIFIED="1734258702229">
+<node TEXT="关卡奖励" ID="ID_1906741029" CREATED="1734258705158" MODIFIED="1734258713693"/>
+<node TEXT="最终奖励" ID="ID_600816108" CREATED="1734258715403" MODIFIED="1734258718295"/>
+</node>
+<node TEXT="死亡规则" ID="ID_1565862230" CREATED="1734259738328" MODIFIED="1734259740637"/>
+</node>
+</node>
+</map>
---
+++ 2024/12/22/谈一下服务端的关卡系统/deadmines.mm
@@ -0,0 +1,120 @@
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="死亡矿井" FOLDED="false" ID="ID_473494821" CREATED="1734170366472" MODIFIED="1734171242903" CHILD_NODES_LAYOUT="TOPTOBOTTOM_RIGHT_CENTERED"><hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false" show_tags="UNDER_NODES" showTagCategories="false" show_icon_for_attributes="true" show_note_icons="true"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#afd3f7" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#afd3f7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<node TEXT="任务" POSITION="bottom_or_right" ID="ID_518745023" CREATED="1734170434108" MODIFIED="1734171242903" HGAP_QUANTITY="38 pt" VSHIFT_QUANTITY="-31.875 pt" CHILD_NODES_LAYOUT="TOPTOBOTTOM_RIGHT_CENTERED">
+<node TEXT="..." ID="ID_499577705" CREATED="1734259050435" MODIFIED="1734259051592"/>
+<node TEXT="暗月马戏团任务" ID="ID_275540888" CREATED="1734171525774" MODIFIED="1734171597318">
+<arrowlink DESTINATION="ID_1952060318" MIDDLE_LABEL="刷新" STARTINCLINATION="230.24999 pt;0 pt;" ENDINCLINATION="344.62499 pt;0 pt;"/>
+</node>
+</node>
+<node TEXT="场景" POSITION="bottom_or_right" ID="ID_249906188" CREATED="1734170407017" MODIFIED="1734171242903" CHILD_NODES_LAYOUT="TOPTOBOTTOM_RIGHT_CENTERED">
+<node TEXT="宝箱" ID="ID_1952060318" CREATED="1734171413383" MODIFIED="1734171417292"/>
+<node TEXT="110" OBJECT="java.lang.Long|110" ID="ID_879269977" CREATED="1734170576448" MODIFIED="1734171242903" CHILD_NODES_LAYOUT="TOPTOBOTTOM_RIGHT_CENTERED"/>
+<node TEXT="火药" ID="ID_1125050120" CREATED="1734170602269" MODIFIED="1734171242903" CHILD_NODES_LAYOUT="TOPTOBOTTOM_RIGHT_CENTERED">
+<node TEXT="拾取刷新精英怪" ID="ID_197032684" CREATED="1734170606210" MODIFIED="1734171242903" CHILD_NODES_LAYOUT="TOPTOBOTTOM_RIGHT_CENTERED"/>
+</node>
+<node TEXT="门" ID="ID_444817609" CREATED="1734171327124" MODIFIED="1734171336561">
+<node TEXT="击杀自动开门" ID="ID_359493655" CREATED="1734171731615" MODIFIED="1734171736572"/>
+<node TEXT="铁甲湾" ID="ID_1706877348" CREATED="1734171776974" MODIFIED="1734171797440">
+<node TEXT="激活火炮" ID="ID_729724069" CREATED="1734171800866" MODIFIED="1734171808206"/>
+<node TEXT="盗贼开锁" ID="ID_1436186019" CREATED="1734171808365" MODIFIED="1734171813378"/>
+</node>
+<node TEXT="初始化" ID="ID_1567165143" CREATED="1734251299446" MODIFIED="1734251303249"/>
+</node>
+<node TEXT="火炮" POSITION="bottom_or_right" ID="ID_1359668248" CREATED="1734170589502" MODIFIED="1734171242903" CHILD_NODES_LAYOUT="TOPTOBOTTOM_RIGHT_CENTERED"/>
+</node>
+<node TEXT="怪物" POSITION="top_or_left" ID="ID_223834890" CREATED="1734170412757" MODIFIED="1734171242903" HGAP_QUANTITY="82.625 pt" VSHIFT_QUANTITY="-51 pt" CHILD_NODES_LAYOUT="TOPTOBOTTOM_RIGHT_CENTERED">
+<node TEXT="重拳先生" ID="ID_1660376722" CREATED="1734170657158" MODIFIED="1734171242903" CHILD_NODES_LAYOUT="TOPTOBOTTOM_RIGHT_CENTERED" HGAP_QUANTITY="21.5 pt" VSHIFT_QUANTITY="-27 pt">
+<node TEXT="切换装备" ID="ID_1806581694" CREATED="1734171128996" MODIFIED="1734171242903" HGAP_QUANTITY="20.75 pt" VSHIFT_QUANTITY="-4.875 pt" CHILD_NODES_LAYOUT="TOPTOBOTTOM_RIGHT_CENTERED">
+<node TEXT="移动到储物箱" ID="ID_458914346" CREATED="1734171164934" MODIFIED="1734171242903" CHILD_NODES_LAYOUT="TOPTOBOTTOM_RIGHT_CENTERED"/>
+</node>
+<node TEXT="释放全局眩晕" ID="ID_1036598176" CREATED="1734171147486" MODIFIED="1734171242903" HGAP_QUANTITY="24.875 pt" CHILD_NODES_LAYOUT="TOPTOBOTTOM_RIGHT_CENTERED"/>
+</node>
+<node TEXT="..." ID="ID_908427348" CREATED="1734259045831" MODIFIED="1734259047156"/>
+</node>
+<node TEXT="副本规则" POSITION="top_or_left" ID="ID_777785078" CREATED="1734258510231" MODIFIED="1734258880345">
+<edge COLOR="#00ff00"/>
+<node TEXT="进入规则" POSITION="bottom_or_right" ID="ID_1576880906" CREATED="1734258530445" MODIFIED="1734258541634">
+<node TEXT="等级" ID="ID_210834421" CREATED="1734258980241" MODIFIED="1734258981707"/>
+</node>
+<node TEXT="离开规则" ID="ID_1579513187" CREATED="1734258676121" MODIFIED="1734258682805">
+<node TEXT="单人" ID="ID_933172799" CREATED="1734259034770" MODIFIED="1734259038930"/>
+<node TEXT="组队" ID="ID_1621019909" CREATED="1734258843563" MODIFIED="1734258847612">
+<node TEXT="队伍解散" ID="ID_598790755" CREATED="1734259010663" MODIFIED="1734259019087"/>
+<node TEXT="队员退队" ID="ID_1511892366" CREATED="1734259020505" MODIFIED="1734259029310"/>
+</node>
+</node>
+</node>
+</node>
+</map>
